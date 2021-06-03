609fd7a6973469eefff8f1a7892972c9ae3d40a4
MANOJ CHALLPALLI <74009435+challapallimanoj@users.noreply.github.com>
2021-06-03 19:57:46 +0530

    final

--- trail/lib/home.dart
+++ trail/lib/home.dart
@@ -4,38 +4,75 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [
-        Wrap(
-          //ROW 1
-          direction: Axis.horizontal,
-          children: [
-            Container(
-              margin: EdgeInsets.all(20.0),
-              child: FlutterLogo(
-                size: 60.0,
+      body: Padding(
+        padding: const EdgeInsets.only(top: 20.0),
+        child: Container(
+          margin: EdgeInsets.only(left: 10.0, right: 10.0),
+          height: 300.0,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: <Widget>[
+              Container(
+                child: Center(
+                  child: Text(
+                    'DJANGO',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 50.0,
+                    ),
+                  ),
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.blue,
+                ),
+                width: 365.0,
               ),
-              height: 250.0,
-              width: 500.0,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(25.0),
-                color: Colors.cyanAccent,
+              SizedBox(
+                width: 20.0,
               ),
-            ),
-            Container(
-              margin: EdgeInsets.all(20.0),
-              child: FlutterLogo(
-                size: 60.0,
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.red,
+                ),
+                width: 365.0,
               ),
-              height: 250.0,
-              width: 500.0,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(25.0),
-                color: Colors.cyanAccent,
+              SizedBox(
+                width: 20.0,
               ),
-            ),
-          ],
+              Container(
+                width: 365.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.green,
+                ),
+              ),
+              SizedBox(
+                width: 20.0,
+              ),
+              Container(
+                width: 365.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.amber,
+                ),
+              ),
+              SizedBox(
+                width: 20.0,
+              ),
+              Container(
+                width: 365.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.orange,
+                ),
+              ),
+            ],
+          ),
         ),
-      ]),
+      ),
     );
   }
 }
