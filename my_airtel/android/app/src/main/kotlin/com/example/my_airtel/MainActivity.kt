9309b99cba87d3df86ff7f6ef88283bb1529d0df
MANOJ CHALLPALLI <74009435+challapallimanoj@users.noreply.github.com>
2021-06-06 13:44:36 +0530

    final

---
+++ my_airtel/android/app/src/main/kotlin/com/example/my_airtel/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.my_airtel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- trail/android/app/src/main/kotlin/com/example/trail/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.trail
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
